c41f1a7b96cf9d785e26b9c3e006189cdb467da8
MaysaraElshewehy <maysara.elshewehy@gmail.com>
2024-11-29 16:27:30 +0200

    cleanup

--- src/libs/io.zig
+++ src/libs/io.zig
@@ -1,52 +1,21 @@
-// This is a min version of https://github.com/Super-ZIG/io library.
-// I created this file to use this library without the need to install it via Zig. 
-// Simply drag this single file into your project and import it.
-//
-// Made with ❤️ by Maysara.
-//
-// Email    : Maysara.Elshewehy@gmail.com
-// GitHub   : https://github.com/maysara-elshewehy
+// • This is a min version of ❮ https://github.com/Super-ZIG/io ❯ library •
+// • • • Simply drag this single file into your project and import it • • •
+// 
+// ╭────────────────────────╮  ✤ https://maysara.xyz
+// │ Made with ❤️ by Maysara │  ✤ https://github.com/maysara-elshewehy
+// ╰────────────────────────╯  ✤ maysara.elshewehy@gmail.com
 
 
+const std = @import("std");
 
-// ╔══════════════════════════════════════ LOAD ══════════════════════════════════════╗
+/// Outputs a simple message followed by a newline.
+pub inline fn out ( comptime _msg: []const u8 ) !void { 
+    try outWith(_msg ++ "\n", .{}); }
 
-    const       std                         = @import("std");
+/// Outputs a formatted message to the console.
+pub inline fn outWith ( comptime _fmt: []const u8, _args: anytype ) !void { 
+    try nosuspend std.io.getStdOut().writer().print(_fmt, _args); }
 
-// ╚══════════════════════════════════════════════════════════════════════════════════╝
-
-
-
-// ╔═════════════════════════════════════ OUTPUT ═════════════════════════════════════╗
-
-    /// Outputs a simple message followed by a newline.
-    pub inline fn out
-    ( comptime _msg: []const u8 ) 
-    !void 
-    {
-        try outWith(_msg ++ "\n", .{});
-    }
-
-    /// Outputs a formatted message to the console.
-    pub inline fn outWith
-    ( comptime _fmt: []const u8, _args: anytype ) 
-    !void 
-    {
-        try nosuspend std.io.getStdOut().writer().print(_fmt, _args);
-    }
-
-// ╚══════════════════════════════════════════════════════════════════════════════════╝
-
-
-
-// ╔══════════════════════════════════════ MORE ══════════════════════════════════════╗
-
-    /// Clears the terminal(Screen).
-    pub inline fn cls
-    () 
-    !void 
-    {
-        _ = try out("\x1b[2J\x1b[H");
-    }
-
-// ╚══════════════════════════════════════════════════════════════════════════════════╝
+/// Clears the terminal(Screen).
+pub inline fn cls () !void {
+    _ = try out("\x1b[2J\x1b[H"); }
--- src/types/all.zig
+++ src/types/all.zig
@@ -1,46 +1,34 @@
-// ╔══════════════════════════════════════ ____ ══════════════════════════════════════╗
+// ╔══════════════════════════════════════ TYPE ══════════════════════════════════════╗
 
-    const       str                         = []const u8;
+    const str = []const u8;
 
-    // array of string
-    pub const   req                         = []const str;
+    /// array of string
+    pub const req = []const str;
 
-    // array of string
-    pub const   opt                         = req;
-
-// ╚══════════════════════════════════════════════════════════════════════════════════╝
-
-
-
-// ╔═══════════════════════════════════════ CMD ══════════════════════════════════════╗
+    /// array of string
+    pub const opt = req;
 
     /// Structure to represent the type of command.
-    pub const   command     = struct 
+    pub const command = struct 
     {
-        name    : str,                          // Name of the command
-        func    : _funcType,                    // Function to execute the command                                               
-        req     : req           = &.{},         // Required options
-        opt     : opt           = &.{},         // Optional options
+        name : str,        // Name of the command
+        func : _funcType,  // Function to execute the command                                               
+        req  : req = &.{}, // Required options
+        opt  : opt = &.{}, // Optional options
         
-        const _funcType         = *const fn ([]const option) bool;
+        const _funcType = *const fn ([]const option) bool;
     };
 
-// ╚══════════════════════════════════════════════════════════════════════════════════╝
-
-
-
-// ╔═══════════════════════════════════════ OPT ══════════════════════════════════════╗
-
     /// Structure to represent the type of option.
-    pub const   option      = struct
+    pub const option = struct
     {
-        name    : str,                          // Name of the option
-        func    : ?_funcType    = undefined,    // Function to execute the option                                               
-        short   : u8,                           // Short form, e.g., -n|-N
-        long    : str,                          // Long form, e.g., --name
-        value   : str           = "",           // Value of the option
+        name  : str,                    // Name of the option
+        func  : ?_funcType = undefined, // Function to execute the option                                               
+        short : u8,                     // Short form, e.g., -n|-N
+        long  : str,                    // Long form, e.g., --name
+        value : str = "",               // Value of the option
 
-        const _funcType         = *const fn (str) bool;
+        const _funcType = *const fn (str) bool;
     };
 
 // ╚══════════════════════════════════════════════════════════════════════════════════╝
